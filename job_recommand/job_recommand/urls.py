971a7112926c9845bbab89d91300c1afdd67ac84
chinlly <118294541+chinlly@users.noreply.github.com>
2023-04-11 16:10:32 +0100

    Your commit message here

--- job_recommand/job_recommand/urls.py
+++ job_recommand/job_recommand/urls.py
@@ -1,12 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
-
-
 urlpatterns = [
-     path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
     path('', include('job_recommand_app.urls'))
-    #path("", views.test),
+    # path("", views.test),
 ]
--- job_recommand/job_recommand_app/views.py
+++ job_recommand/job_recommand_app/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from .forms import ProfileForm
 import requests
 
+
 # Create your views here.
 # def test(request):
 #     user = Account(name="chinlly", pwd="123456")
@@ -17,7 +18,6 @@ import requests
 
 
 def homePage(request):
-
     url = "https://api.adzuna.com/v1/api/jobs/us/search/1"
 
     params = {
@@ -31,18 +31,20 @@ def homePage(request):
     data = response.json()
     results = data['results']
 
-
     job_listings = []
     for result in results:
         job = {"title": result['title'], "location": result['location']['area'],
                "company": result['company']['display_name'], "url": result['redirect_url'],
-               "description":result['description']}
+               "description": result['description']}
         job_listings.append(job)
 
-
-
+    print(job_listings)
 
     return render(request, 'home.html', {'job_listings': job_listings})
+
+
+def indexPage(request):
+    return render(request, 'login.html')
 
 
 def registerPage(request):
@@ -53,18 +55,22 @@ def registerPage(request):
         # if User.objects.filter(userame = username):
         #     messages.error(request, "username has already exists")
 
-        if len(password) > 10 or len(password)<8:
+        if len(password) > 10 or len(password) < 8:
             messages.error(request, "password must be under 10 and over 8 characters ")
             return redirect('register')
+        elif User.objects.filter(username=username).exists():
+            return render(request, 'register.html', {'error': 'Username is already taken.'})
 
-        user = User.objects.create_user(username = username, password = password)
+        user = User.objects.create_user(username=username, password=password)
         user.save()
         messages.success(request, "your Account has been created successfully")
-        return redirect('login')
+        return render(request, 'login.html')
+
     return render(request, 'register.html')
 
 
 def loginPage(request):
+
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -73,17 +79,19 @@ def loginPage(request):
 
         if user is not None:  # if it is authenticated
             login(request, user)
-            return render(request, "index.html",{'username':username})
+            print(request.method)
+            return render(request, "index.html", {'username': username})
         else:
             messages.error(request, "you are not authenicated")
-            return redirect('login')
+            return render(request, 'home.html', {'user': user})
 
     return render(request, 'login.html')
 
+
 def signout(request):
     logout(request)
-    messages.success(request,"you have successfully logged out")
-    return redirect('index')
+    messages.success(request, "you have successfully logged out")
+    return redirect('login')
 
 
 def profile(request):
@@ -94,4 +102,4 @@ def profile(request):
         # return render(request,"post/new.html",{"form":form} )
     else:
         form = ProfileForm()
-    return render(request,"profile.html",{"form":form} )
+    return render(request, "profile.html", {"form": form})
